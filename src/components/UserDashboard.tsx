79135a15f60e11304e35dbea64628f685777a7f4
Kaiyue Lin <lin.kaiyu@northeastern.edu>
2025-05-30 21:55:25 -0700

    feat: created new components

---
+++ src/components/UserDashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// 用户栏是点击右上角头像之后弹出来的内容
+// 悬浮窗口
+export type UserProps = {
+    avatar: string;
+};
+
+export const UserDashBoard = () => {
+    return (
+        <></>
+    )
+};
