7c513a48358f54a010fd9235728ce2afaced6193
Kaiyue Lin <lin.kaiyu@northeastern.edu>
2025-05-30 21:54:42 -0700

    feat: prettier formatted

--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -1,40 +1,44 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 
-export const ImageLoader = ({src, ...props}) => {
+export const ImageLoader = ({ src, ...props }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<boolean>(false);
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
 
-    useEffect(() => {
-        setLoading(true);
-        setError(false);
+    const imgInstance = new Image();
+    imgInstance.src = src;
 
-        const imgInstance = new Image();
-        imgInstance.src = src;
-        
-        imgInstance.onload = () => {
-            setLoading(false);
-            setError(false);
-        };
+    imgInstance.onload = () => {
+      setLoading(false);
+      setError(false);
+    };
 
-        imgInstance.onerror = () => {
-            setLoading(false);
-            setError(true);
-        };
+    imgInstance.onerror = () => {
+      setLoading(false);
+      setError(true);
+    };
+  }, [src]);
 
-    }, [src])
-    
-    // loading && !error : loading page
-    // error: err page
-    // !loading && !error successfully loaded
-    return (
-        <>
-            {loading && !error ? 
-                <img src="loading.png" 
-                    className={`flex-none animate-pulse bg-gray-200 ${props.className}`}></img> 
-                : null}
-            {error ? <img src="error-404.png" className={props.className}></img> : null}
-            {!loading && !error ? <img src={src} className={props.className}></img> : null}
-        </>
-    )
-}
+  // loading && !error : loading page
+  // error: err page
+  // !loading && !error successfully loaded
+  return (
+    <>
+      {loading && !error ? (
+        <img
+          src="loading.png"
+          className={`flex-none animate-pulse bg-gray-200 ${props.className}`}
+        ></img>
+      ) : null}
+      {error ? (
+        <img src="error-404.png" className={props.className}></img>
+      ) : null}
+      {!loading && !error ? (
+        <img src={src} className={props.className}></img>
+      ) : null}
+    </>
+  );
+};
--- src/components/LoadingNote.tsx
+++ src/components/LoadingNote.tsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 
 export const LoadingNote = () => {
-    return (
-        // <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="basis-64">
-            <div className="mx-auto w-full max-w-md rounded-md border border-blue-300 p-4">
-                <div className="flex animate-pulse space-x-4">
-                    <div className="size-10 rounded-full bg-gray-200"></div>
-                    <div className="flex-1 space-y-6 py-1">
-                        <div className="h-2 rounded bg-gray-200"></div>
-                        <div className="space-y-3">
-                            <div className="grid grid-cols-3 gap-4">
-                                <div className="col-span-2 h-2 rounded bg-gray-200"></div>
-                                <div className="col-span-1 h-2 rounded bg-gray-200"></div>
-                            </div>
-                            <div className="h-2 rounded bg-gray-200"></div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    // <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="basis-64">
+      <div className="mx-auto w-full max-w-md rounded-md border-3 border-indigo-300 p-4">
+        <div className="flex animate-pulse space-x-4">
+          <div className="size-10 rounded-full bg-gray-400"></div>
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 rounded bg-gray-400"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="col-span-2 h-2 rounded bg-gray-400"></div>
+                <div className="col-span-1 h-2 rounded bg-gray-400"></div>
+              </div>
+              <div className="h-2 rounded bg-gray-400"></div>
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
