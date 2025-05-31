873b5c56f008d6a5382cdec0ce435744235d97c2
Kaiyue Lin <lin.kaiyu@northeastern.edu>
2025-05-30 21:55:13 -0700

    feat: implemented UI styling

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,38 +1,42 @@
 import React from 'react';
 
 export type NoteType = {
-    noteId: string;
-    title: string;
-    content: string;
-    // 下面两个只能有一个（需要限制）
-    pictureLinks: string[] | null;
-    videoLink: string | null;
+  noteId: string;
+  title: string;
+  content: string;
+  // 下面两个只能有一个（需要限制）
+  pictureLinks?: string[] | null;
+  videoLink?: string | null;
 };
 
-export function Note(props: {note: NoteType, handleNoteClick: (note: NoteType) => void}) {
-    const handleClick = () => {
-        props.handleNoteClick(props.note)
-    }
+export function Note(props: {
+  note: NoteType;
+  handleNoteClick: (note: NoteType) => void;
+}) {
+  const handleClick = () => {
+    props.handleNoteClick(props.note);
+  };
 
-    return (<div
-        key={props.note.noteId}
-        className="bg-gray-50 p-4 rounded-lg shadow-sm hover:shadow-md 
+  return (
+    <div
+      className="bg-gray-50 p-4 rounded-lg shadow-sm hover:shadow-md 
         transition-shadow duration-200 border border-gray-200"
     >
-        <h3 className="text-lg font-semibold text-gray-700 mb-2">
-            {props.note.title}
-        </h3>
-        <p className="text-sm text-gray-600">
-            {props.note.content}
-        </p>
-        
-        <button
-            className="
-            mt-3 px-3 py-1 bg-indigo-400 text-white text-sm rounded-md 
-            hover:bg-indigo-600 transition-colors duration-200"
-            onClick={handleClick}
-        >
-            View Details
-        </button>
-    </div>)
+      <h3 className="text-lg font-semibold text-gray-700 mb-2">
+        {props.note.title}
+      </h3>
+      <p className="text-sm text-gray-600 line-clamp-3 py-1">
+        {props.note.content}
+      </p>
+
+      <button
+        className="
+            mt-3 px-3 py-1 bg-indigo-500 text-white text-sm rounded-md 
+            hover:bg-indigo-700 transition-colors duration-200"
+        onClick={handleClick}
+      >
+        View Details
+      </button>
+    </div>
+  );
 }
--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -1,62 +1,74 @@
-import React, {useState} from "react"
-import { NoteType } from "./Note"
-import { ImageLoader } from "./ImageLoader"
+import React, {useState} from 'react';
+import { NoteType } from './Note';
+import { ImageLoader } from './ImageLoader';
 
-export const NoteDetail = (props: {note: NoteType, onClickClose: () => void}) => {
-    
+export const NoteDetail = (props: {
+  note: NoteType;
+  onClickClose: () => void;
+}) => {
+  const [like, setLike] = useState<number>(50);
 
-    return (
-        <div
-            key = {props.note.noteId}
-            className="absolute left-1/2 top-1/2 overflow-visible
-                        w-1/4
-                        bg-gray-50 rounded-lg shadow-lg p-6
-                        transform -translate-y-1/2 *:z-10">
-            <button
-                onClick={props.onClickClose}
-                className="
-                    absolute right-0 top-0
-                    mx-4 my-4 px-2 py-2
-                    bg-indigo-400 text-white font-semibold
-                    rounded-l-full rounded-r-full shadow-lg
-                    hover:bg-indigo-600
-                    focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-75
-                    transition-all duration-300 ease-in-out
-                    whitespace-nowrap              
-                    "
-            >
-                <img src="close.png" className='h-5 w-5'/>
-            </button>
-            <div className="text-2xl font-bold mb-4">{props.note.title}</div>
-            <div className="flex overflow-x-auto max-h-96 mb-6">{props.note.content}</div>
+  return (
+    <div className='relative bg-gray-200 rounded-lg shadow-lg 
+                    px-4 py-6 z-0'
+      key={props.note.noteId}
+    >
+      <button
+        onClick={props.onClickClose}
+        className="
+          absolute  z-1 right-0 top-0
+          mx-3 my-3 px-2 py-2
+          bg-indigo-400 text-white font-semibold
+          rounded-l-full rounded-r-full shadow-lg
+          hover:bg-indigo-600
+          focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-75
+          transition-all duration-300 ease-in-out
+          whitespace-nowrap              
+          "
+      >
+        <img src="close.png" className="h-5 w-5" />
+      </button>
 
-            <div className="h-10px"></div>
+      <div className='bg-gray-100 rounded-lg shadow-lg p-6'>
+      
+        <div className="text-2xl font-bold mb-2">{props.note.title}</div>
 
-            {true ? (
-            <div className="flex overflow-x-auto space-x-4 pb-2">
-                {props.note.pictureLinks ? props.note.pictureLinks.map((link: string, idx: number) => (
-                    <ImageLoader 
-                        key={link.length > 5 ? link.slice(link.length-5, link.length): `key-${idx}`}
-                        src="https://www.bushheritage.org.au/cdn-cgi/image/quality=90,fit=scale-down/https://www.bushheritage.org.au/uploads/main/Images/Places/Qld/pilungah/RS32891-kanga-pilung-peter-wallis.jpg"
-                        className="w-32 h-32 object-cover rounded-md"
-                />
-                )) : null}
-               
-               <ImageLoader 
-                    src="localhost:11999"
-                    className="w-32 h-32 object-cover rounded-md"
-                />
-                <ImageLoader 
-                    src="https://nationalzoo.si.edu/sites/default/files/styles/wide/public/2024-11/20241024-817A7713-16RP-bao-li.jpg?h=6acbff97&itok=orzUN1IX"
-                    className="w-32 h-32 object-cover rounded-md"
-                />
-                <ImageLoader 
-                    src="https://www.bushheritage.org.au/cdn-cgi/image/quality=90,fit=scale-down/https://www.bushheritage.org.au/uploads/main/Images/Places/Qld/pilungah/RS32891-kanga-pilung-peter-wallis.jpg"
-                    className="w-32 h-32 object-cover rounded-md"
-                />
-            </div>
-            ) : null}
-            {props.note.videoLink ? <div></div> : null}
+        <hr className="mb-3 bg-gray-800 mr-5"></hr>
+
+        <div className="flex overflow-x-auto max-h-96 mb-6">
+          {props.note.content}
         </div>
-    )
-}
+
+        {props.note.pictureLinks ? (
+          <div className="flex overflow-x-auto space-x-4 pb-2">
+            {props.note.pictureLinks
+              ? props.note.pictureLinks.map((link: string, idx: number) => (
+                  <ImageLoader
+                    key={
+                      link.length > 5
+                        ? link.slice(link.length - 5, link.length)
+                        : `key-${idx}`
+                    }
+                    src={link}
+                    className="w-32 h-32 object-cover rounded-md"
+                  />
+                ))
+              : null}
+          </div>
+        ) : null}
+        {props.note.videoLink ? <div></div> : null}
+
+      
+        <div className='w-full mt-3 bg-gray-200 flex'>
+          <button className='flex'>
+            <img src="love.png" onClick={() => setLike(like+1)}
+                  className='bg-red-300 w-5 h-5 rounded-md
+                            hover:bg-red-500' />
+             : {like} 
+          </button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
