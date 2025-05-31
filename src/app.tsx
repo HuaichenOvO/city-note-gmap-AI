e35a7316c8ff394800158b7f1c14c8fd6a5c3cc3
Kaiyue Lin <lin.kaiyu@northeastern.edu>
2025-05-30 21:55:25 -0700

    feat: created new components

--- src/app.tsx
+++ src/app.tsx
@@ -18,6 +18,7 @@
 import React, { useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { NavBar } from './components/NavBar';
 import { NoteType } from './components/Note';
 import { NoteContainer } from './components/NoteContainer';
 import { MapComponent } from './components/MapComponent';
@@ -27,48 +28,126 @@ import { GMAP_API_KEY, GMAP_MAP_ID } from '../env';
 import { CITY_JSON } from '../pj_config';
 
 const App: React.FC = () => {
-    const [countyNameState, setCountyNameState] = useState<string | null>(null);
-    const [noteDetailState, setNoteDetailState] = useState<NoteType | null>(null);
-    const [noteVisibleState, setNoteVisibleState] = useState<boolean>(false);
+  const [countyNameState, setCountyNameState] = useState<string | null>(null);
+  const [noteDetailDataState, setNoteDetailDataState] =
+    useState<NoteType | null>(null);
+  const [noteDetailVisibleState, setNoteDetailVisibleState] =
+    useState<boolean>(false);
+  const [notePageVisibleState, setNotePageVisibleState] =
+    useState<boolean>(true);
 
-    // TODO: set default county name as user's located county or New York
-    const handleNoteClick = (note: NoteType) => {
-        setNoteVisibleState(true);
-        setNoteDetailState(note);
+  const handleCountySelect = (county: string | null) => {
+    if (county != countyNameState) {
+      setNoteDetailDataState(null);
     }
+    setCountyNameState(county);
+    setNoteDetailVisibleState(false);
+    setNotePageVisibleState(true);
+  };
 
-    const notes: NoteType[] = Array.from({ length: 20 }, (_, i) => ({
-        noteId: `${i}`,
-        title: `Event ${i + 1} in ${countyNameState}`,
-        content: `This is a detailed description for card number ${
-            i + 1
-        }. It can be quite long to test scrolling behavior.`,
-        pictureLinks: null, videoLink: null
-    }));
+  const handleMapDrag = () => {
+    setNoteDetailVisibleState(false);
+  };
 
-    return (
-        <div className="static w-full h-full">
-            <MapComponent
-                GMAP_API_KEY={GMAP_API_KEY}
-                GMAP_MAP_ID={GMAP_MAP_ID}
-                GEO_JSON_URL={CITY_JSON}
-                onCountySelect={setCountyNameState}
-            />
-            {noteVisibleState && noteDetailState ? 
-                <NoteDetail {...{note: noteDetailState, 
-                    onClickClose: () => setNoteVisibleState(false)}}/>
-                : null
-            }
-            <NoteContainer countyName={countyNameState} notes={(notes ? notes : [])} 
-                handleNoteClick={handleNoteClick} />
+  const handleDetailPageClose = () => {
+    setNoteDetailVisibleState(false);
+  };
+
+  const handleSidePageClose = (toggleState: boolean) => {
+    setNoteDetailVisibleState(false);
+    setNotePageVisibleState(toggleState);
+  };
+
+  // TODO: set default county name as user's located county or New York
+  const handleNoteClick = (note: NoteType) => {
+    setNoteDetailVisibleState(true);
+    setNoteDetailDataState(note);
+  };
+
+  const notes: NoteType[] = Array.from({ length: 20 }, (_, i) => ({
+    noteId: `${i}`,
+    title: `Event ${i + 1} in ${countyNameState}`,
+    content:
+      i > 3
+        ? `This is a detailed description for card number ${i + 1}. 
+        It can be quite long to test scrolling behavior.`
+        : 'Kangaroos are famous for their forward-opening pouchwhere the \
+        joey (baby kangaroo) develops and suckles. A female kangaroo is \
+        known as a flyer or a doe and a male kangaroo a buck or a boomer \
+        (hence the nickname of the  \
+        Australian mens basketball team, the Boomers). They live in social \
+        groups called mobs. Can kangaroos swim? Yes! Kangaroos are reasonably \
+        strong swimmers. Though they re not exactly built for swimming, they \
+        can comfortably swim short distances if motivated enough to access \
+        new pastures or to avoid predators Kangaroos are famous for their forward \
+        -opening pouchwhere the joey (baby kangaroo) develops and suckles. A \
+        female kangaroo is known as a flyer or a doe and a male kangaroo a buck or a boomer \
+        (hence the nickname of the Australian mens basketball team, the Boomers). \
+        They live in social groups called mobs. Can kangaroos swim? Yes! Kangaroos \
+        are reasonably strong swimmers. Though they re not exactly built for swimming, \
+        they can comfortably swim short distances if motivated enough to access \
+        new pastures or to avoid predators',
+    pictureLinks:
+      i < 5
+        ? [
+            'https://nationalzoo.si.edu/sites/default/files/styles/wide/public/2024-11/20241024-817A7713-16RP-bao-li.jpg?h=6acbff97&itok=orzUN1IX',
+            'localhost:11999',
+            'https://www.bushheritage.org.au/cdn-cgi/image/quality=90,fit=scale-down/https://www.bushheritage.org.au/uploads/main/Images/Places/Qld/pilungah/RS32891-kanga-pilung-peter-wallis.jpg',
+          ]
+        : [],
+    videoLink: null,
+  }));
+
+  return (
+    <>
+      <div className="h-1/20">
+        <NavBar/>
+        {` County name: ${countyNameState}, 
+        Which note is displaying? ${noteDetailDataState?.title}
+        Is the note visible now? ${noteDetailVisibleState}
+        Is the side page on? ${notePageVisibleState}`}
+      </div>
+
+      <div className="relative flex flex-row w-full h-19/20 z-1">
+
+          {noteDetailVisibleState && noteDetailDataState 
+          ? (
+            <div className="absolute left-1/2 top-1/2 overflow-visible w-1/4
+                            transform -translate-y-1/2 z-10">
+              <NoteDetail {...{
+                    note: noteDetailDataState,
+                    onClickClose: handleDetailPageClose,
+                }}/>
+            </div>
+          ) : null}
+        
+        <NoteContainer
+          countyName={countyNameState}
+          notes={notes ? notes : []}
+          visible={notePageVisibleState}
+          handleNoteClick={handleNoteClick}
+          handlePageClose={handleSidePageClose}
+        />
+
+        <div className='relative z-2 basis-full right-0 
+                        rounded-2xl bg-gray-100 p-2'>
+          <MapComponent
+            GMAP_API_KEY={GMAP_API_KEY}
+            GMAP_MAP_ID={GMAP_MAP_ID}
+            GEO_JSON_URL={CITY_JSON}
+            onCountySelect={handleCountySelect}
+            onBoundaryDrag={handleMapDrag}
+          />
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default App;
 
 const container = document.getElementById('app');
 if (container) {
-    const root = createRoot(container);
-    root.render(<App />);
+  const root = createRoot(container);
+  root.render(<App />);
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export type NavBarProps = {
+    avatarUrl: string;
+    userName: string;
+};
+
+export const NavBar = ({...props}) => {
+    return (
+        <>NavBar</>
+    )
+};
