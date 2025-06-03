23b99cb2e40edf91d4e535971741bf816f5330b2
chrischenlixing <chrischenlixing@gmail.com>
2025-06-02 20:30:34 -0700

    crud login

---
+++ backend/src/main/java/com/citynote/CityNoteApplication.java
@@ -0,0 +1,11 @@
+package com.citynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityNoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CityNoteApplication.class, args);
+    }
+} 
