d48a8aca1a3148ec39c78ee92f0e3f69b5311f6f
Kaiyue Lin <lin.kaiyu@northeastern.edu>
2025-06-26 22:23:07 -0700

    feat: updated services and controllers

--- backend/src/main/java/com/citynote/service/EventService.java
+++ backend/src/main/java/com/citynote/service/EventService.java
@@ -2,6 +2,8 @@ package com.citynote.service;
 
 import com.citynote.dto.EventResponseDTO;
 import com.citynote.dto.EventRequestDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +12,19 @@ import java.util.Optional;
 @Service
 public interface EventService {
     Optional<EventResponseDTO> getEventById(int id);
-    List<EventResponseDTO> getEventsByCounty(String countyId); // a list of county-aggregated
-    List<EventResponseDTO> getUserPostedEvents(Long userId); // a list of user-posted history
-    int postEvent(EventRequestDTO eventRequestDTO); // return new event's ID
-    int updateEvent(EventRequestDTO eventRequestDTO); // return new event's ID
-    Boolean deleteEvent(int eventId); // true if successfully deleted
+
+    // a list of county-aggregated
+    Page<EventResponseDTO> getPagesOfEventsByCounty(int countyId, Pageable pageable);
+
+    // a list of user-posted history
+    Page<EventResponseDTO> getPagesOfUserPostedEvents(Long userId, Pageable pageable);
+
+    // return new event's ID
+    int postEvent(EventRequestDTO eventRequestDTO);
+
+    // get id from controller path variable, return new event's ID
+    int updateEvent(int eventId, EventRequestDTO eventRequestDTO);
+
+    // true if successfully deleted
+    Boolean deleteEvent(int eventId);
 }
